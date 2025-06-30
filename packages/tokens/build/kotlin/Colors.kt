f373b3986b181e70580a91cd5ea9e3e7e949ffea
corollazero <wovk1010@gmail.com>
2025-06-30 23:44:38 +0900

    style dictionary 빌드 결과물 예시

---
+++ packages/tokens/build/kotlin/Colors.kt
@@ -0,0 +1,16 @@
+
+
+// Do not edit directly, this file was auto-generated.
+
+
+
+package 
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+object {
+  val colorBackgroundPrimary = #FFFFFF
+  val colorBrandPrimary = #00A3FF
+  val colorFontPrimary = #111111
+}
